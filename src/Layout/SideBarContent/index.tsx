a5e736f7548d0eea5a029a48e3dee215a587e426
vynguyenle <vylenguyen@tcom.vn>
2025-05-15 16:54:44 +0700

    fix: side bar

--- src/Layout/SideBarContent/index.tsx
+++ src/Layout/SideBarContent/index.tsx
@@ -1,7 +1,84 @@
-import { Button, Col, Row } from "antd";
+import { Button, Col, Menu, Row } from "antd";
 import FormWrap from "../../Components/Form/FormWrap";
 import "./sideBar.scss";
+import { AppstoreOutlined, MailOutlined } from "@ant-design/icons";
+import { MenuProps } from "antd/lib";
+import { useState } from "react";
+
+interface LevelKeysProps {
+  key?: string;
+  children?: LevelKeysProps[];
+}
+
+type MenuItem = Required<MenuProps>["items"][number];
+
+const items: MenuItem[] = [
+  {
+    key: "1",
+    icon: (
+      <img
+        width="32"
+        height="32"
+        src="https://img.icons8.com/windows/32/company.png"
+        alt="company"
+      />
+    ),
+    label: "Địa điểm",
+  },
+  {
+    key: "2",
+    icon: (
+      <img
+        width="32"
+        height="32"
+        src="https://img.icons8.com/small/32/company.png"
+        alt="company"
+      />
+    ),
+    label: "Chỉnh sửa địa điểm",
+    children: [
+      { key: "11", label: "Thông tin chi tiết" },
+      { key: "12", label: "Thông tin khách" },
+      { key: "13", label: "Chính sách" },
+      { key: "14", label: "Giờ thuê" },
+    ],
+  },
+];
+
+const getLevelKeys = (items1: LevelKeysProps[]) => {
+  const key: Record<string, number> = {};
+  const func = (items2: LevelKeysProps[], level = 1) => {
+    items2.forEach((item) => {
+      if (item.key) {
+        key[item.key] = level;
+      }
+      if (item.children) {
+        func(item.children, level + 1);
+      }
+    });
+  };
+  func(items1);
+  return key;
+};
+
 export const SidebarContent = () => {
+  const [stateOpenKeys, setStateOpenKeys] = useState(["2"]);
+  const [selectedKeys, setSelectedKeys] = useState<string[]>(["11"]);
+
+  const onOpenChange: MenuProps["onOpenChange"] = (openKeys) => {
+    const currentOpenKey = openKeys.find(
+      (key) => stateOpenKeys.indexOf(key) === -1
+    );
+    if (currentOpenKey === "2") {
+      setSelectedKeys(["11"]);
+    }
+    setStateOpenKeys(openKeys);
+  };
+
+  const onSelect: MenuProps["onSelect"] = ({ key }) => {
+    setSelectedKeys([key]);
+  };
+
   return (
     <div className="side_bar">
       <FormWrap className="side_bar-form">
@@ -25,6 +102,17 @@ export const SidebarContent = () => {
             </div>
           </div>
         </Row>
+        <div className="side_bar-menu">
+          <Menu
+            mode="inline"
+            selectedKeys={selectedKeys}
+            openKeys={stateOpenKeys}
+            onOpenChange={onOpenChange}
+            onSelect={onSelect}
+            style={{ width: 256 }}
+            items={items}
+          />
+        </div>
       </FormWrap>
     </div>
   );

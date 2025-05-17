f7387c4d9fa5dea0650d74f1ce30fe4453a9b193
vynguyenle <vylenguyen@tcom.vn>
2025-05-17 14:53:00 +0700

    fix: code

--- src/LayoutOption/ScrollSpyLayout/index.tsx
+++ src/LayoutOption/ScrollSpyLayout/index.tsx
@@ -72,8 +72,8 @@ const ScrollSpyLayout: React.FC<ScrollSpyLayoutProps> = ({
             }
           },
           {
-            rootMargin: "-50px 0px -40% 0px",
-            threshold: [0, 0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9, 1.0],
+            rootMargin: "-20% 0px -20% 0px",
+            threshold: [0.5, 0.75],
           }
         );
 
@@ -112,8 +112,8 @@ const ScrollSpyLayout: React.FC<ScrollSpyLayoutProps> = ({
         }
       },
       {
-        rootMargin: "-50px 0px -40% 0px",
-        threshold: [0, 0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9, 1.0],
+        rootMargin: "-20% 0px -20% 0px",
+        threshold: [0.5, 0.75],
       }
     );
 
